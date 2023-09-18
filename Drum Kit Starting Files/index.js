074a20530bdd147fedd7ed6e217548c24a11feb8
Ananya Priyadarshini <143418973+ananyap05@users.noreply.github.com>
2023-09-18 15:55:24 +0530

    Add files via upload
    I have created a Drum Kit by using Audio and Javascript.

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,56 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonInnerHTML=this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+        var crash=new Audio("sounds/crash.mp3");
+        crash.play();
+      break;
+    case "a":
+      var kick=new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare=new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1=new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2=new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3=new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4=new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(key){
+  var addClass=document.querySelector("."+key);
+  addClass.classList.add("pressed");
+  setTimeout(function(){
+    addClass.classList.remove("pressed");
+  },100);
+}
